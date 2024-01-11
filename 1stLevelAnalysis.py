bbd313de98911ded63071d79b5f4296030d04934
angietep <angelestepper@gmail.com>
2024-01-11 11:26:53 -0300

    first level

--- 1stLevelAnalysis.py
+++ 1stLevelAnalysis.py
@@ -40,10 +40,10 @@ def main ():
     if hasattr(sys, "ps1"):
         options = {}
         workdir = os.environ["ROOTDIR"]
-        rawdata = os.path.join(workdir,"fmriprep_examples")
+        rawdata = os.path.join("/Volumes/TOSHIBA/Preprocessed/FEPtrt_bids")
         #derivat = os.path.join(workdir,"derivatives","fmriprep-example")
-        output  = os.path.join(rawdata,"firstlevel")
-        masks = os.path.join(rawdata,"masks")
+        output  = os.path.join(workdir,"firstlevel")
+        masks = os.path.join("/Volumes/TOSHIBA/masks")
         participants = []
 
     else :
@@ -56,17 +56,17 @@ def main ():
         script_path = os.path.dirname(__file__)
         masks = os.path.join(script_path,"masks")
 
-    seednames = ['DCPutamen_space-MNI152NLin2009cAsym.nii.gz',
-                 'DorsalCaudate_space-MNI152NLin2009cAsym.nii.gz',
-                 'InfVentralCaudate_space-MNI152NLin2009cAsym.nii.gz',
-                 'SupVentralCaudate_space-MNI152NLin2009cAsym.nii.gz',
-                 'VRPutamen_space-MNI152NLin2009cAsym.nii.gz',
-                 'DRPutamen_space-MNI152NLin2009cAsym.nii.gz']
+    seednames = ['DCPutamen_space-MNI152_dim-9110991.nii.gz',
+                 'DorsalCaudate_space-MNI152_dim-9110991.nii.gz',
+                 'InfVentralCaudate_space-MNI152_dim-9110991.nii.gz',
+                 'SupVentralCaudate_space-MNI152_dim-9110991.nii.gz',
+                 'VRPutamen_space-MNI152_dim-9110991.nii.gz',
+                 'DRPutamen_space-MNI152_dim-9110991.nii.gz']
 
     print('Rawdata: ', rawdata)
 
     #################
-    Vgm_img = nib.load(os.path.join(masks,'GrayMattermask_thalamus_space-MNI152NLin2009cAsym.nii.gz'))
+    Vgm_img = nib.load(os.path.join(masks,'GrayMattermask_thalamus_space-MNI152_dim-9110991.nii.gz'))
     #extract data array
     Vgm = Vgm_img.get_fdata()
     #save origianl dimensions (voxels_x, voxels_y, voxels_z)
@@ -76,7 +76,7 @@ def main ():
     #save indexes in which Vgm == 1 (indexes for gray matter location)
     idx_GM = np.where(Vgm)[1]
 
-    bidslayout = bids.BIDSLayout(derivat,derivatives=True, validate = False) #With validate = True it doesn't find any subjects
+    bidslayout = bids.BIDSLayout(rawdata,derivatives=False, validate = False) #With validate = True it doesn't find any subjects
 
     if not participants:
         participants = bidslayout.get_subjects()
@@ -86,198 +86,136 @@ def main ():
         for ses in bidslayout.get_sessions(subject=i):
             print("Session: ", ses)
             #for multi-echo files, apply transform to standard space
-            for r in bidslayout.get_runs(subject=i, session=ses):
-                print("Run:", r)
-                filesrest = bidslayout.get(subject=i,
-                                 session=ses,
-                                 run=r,
-                                 extension=".nii.gz",
-                                 suffix="bold",
-                                 space="MNI152NLin2009cAsym",
-                                 #regex_search=True,
-                                 #invalid_filters="allow"
-                                 )
+            filesrest = bidslayout.get(subject=i,
+                             session=ses,
+                             extension=".nii.gz",
+                             suffix="bold",
+                             #regex_search=True,
+                             #invalid_filters="allow"
+                             )
 
-                print("Filerest:", filesrest)
-                if len(filesrest)>1:
-                    print(f"More than one file match bold filters. Subject {i}, ses {ses}, run {r}")
-                    continue
-                elif len(filesrest)<1:
-                    print(f"No file match bold filters. Subject {i}, ses {ses}, run {r}")
-                    continue
-                bold = filesrest[0]
+            print("Filerest:", filesrest)
+            if len(filesrest)>1:
+                print(f"More than one file match bold filters. Subject {i}, ses {ses}")
+                continue
+            elif len(filesrest)<1:
+                print(f"No file match bold filters. Subject {i}, ses {ses}")
+                continue
+            bold = filesrest[0]
 
-                filesbrainmask = bidslayout.get(subject=i,
-                                  session=ses,
-                                  run=r,
-                                  task='rest',
-                                  extension=".nii.gz",
-                                  suffix="mask",
-                                  space="MNI152NLin2009cAsym",
-                                  #regex_search=True,
-                                  invalid_filters="allow"
-                                  )
-                print("Brain_Mask:", filesbrainmask)
-                if len(filesbrainmask)>1:
-                    print(f"More than one file match brainmask filters. Subject {i}, ses {ses}, run {r}")
-                    continue
-                brainmask = filesbrainmask[0]
+            filesanat = bidslayout.get(subject=i,
+                             session=ses,
+                             extension=".nii.gz",
+                             suffix="T1w",
+                             #desc="preproc",
+                             #regex_search=True,
+                             invalid_filters="allow"
+                             )
 
+            print(f"Anat: {filesanat} ")
+            if len(filesrest)>1:
+                print(f"More than one file match anat filters. Subject {i}, ses {ses}")
+                continue
+            #anat = filesanat[0]
 
-                filesanat = bidslayout.get(subject=i,
-                                 session=ses,
-                                 run=r,
-                                 extension=".nii.gz",
-                                 suffix="T1w",
-                                 #desc="preproc",
-                                 space="MNI152NLin2009cAsym",
-                                 #regex_search=True,
-                                 invalid_filters="allow"
-                                 )
+            #subj_i=participants.index(i)
+            t = np.zeros((len(idx_GM), len(seednames)))
 
-                print(f"Anat: {filesanat} ")
-                if len(filesrest)>1:
-                    print(f"More than one file match anat filters. Subject {i}, ses {ses}, run {r}")
-                    continue
-                #anat = filesanat[0]
+            #bold data
+            func_img = nimg.load_img(bold)
+            dim4d = func_img.shape
+            N = dim4d[-1]  # number of time points
 
-                #subj_i=participants.index(i)
-                t = np.zeros((len(idx_GM), len(seednames)))
+            # #reshape into 2d
+            func_2d = func_img.get_fdata().reshape(-1, func_img.shape[-1]).T
 
-                #Load Confounds
-                confounds_ents = {}
-                confounds_ents["subject"] = bold.entities['subject']
-                confounds_ents["session"] = bold.entities['session']
-                #confounds_ents["acquisition"] = bold.entities['acquisition']
-                confounds_ents["run"] = bold.entities['run']
-                confounds_ents["task"] = bold.entities['TaskName']
-                #confounds_ents['desc'] = "confounds"
-                confounds_ents['suffix'] = "timeseries"
-                confounds_ents['extension'] = ".tsv"
-                confounds = bidslayout.get(return_type='file', **confounds_ents, invalid_filters="allow")[0]
-                allconfounds_df = pd.read_csv(confounds, sep='\t')
+            # pre-allocate space for seeds' time series
+            TS_seeds = np.empty((N, len(seednames)))
+            # Input new nifti for seeds
+            # Compute SEEDs TS
+            for k in range(len(seednames)):
+                print('----- TS Seed:', seednames[k], '-----')
 
-                melodic_ents = confounds_ents
-                melodic_ents['suffix'] = "mixing"
-                melodic = bidslayout.get(return_type='file', **melodic_ents, invalid_filters="allow")[0]
-                melodic_df = pd.read_csv(melodic, sep='\t')
+                seed = os.path.join(masks, seednames[k])
+                #load seed image
+                Vseed_img = nib.load(seed)
+                Vseed = Vseed_img.get_fdata().reshape(func_2d.shape[-1])
 
-                aroma_noise = confounds_ents
-                aroma_noise['suffix'] = "AROMAnoiseICs"
-                aroma_noise['extension'] = ".csv"
-                aroma_noise = bidslayout.get(return_type='file', **aroma_noise, invalid_filters="allow")[0]
-                aroma_noise_idx = np.genfromtxt(aroma_noise, delimiter=',').astype(int)
+                #for each time point, compute mean of voxels in which seed==1
+                for vol in range(N):
+                    Vaux = func_2d[vol, :]
+                    TS_seeds[vol, k] = np.mean(Vaux[Vseed == 1])
+                    del Vaux
 
-                aroma_conf = melodic_df.iloc[:,aroma_noise_idx-1]
+                del Vseed
 
-                conf_index= ['csf', 'white_matter','cosine00','cosine01','cosine02','cosine03']
-                confounds_df = allconfounds_df[conf_index]
+            # 1st Level Analysis: TSvox = b0 + b1*TSseed1 + b2*TSseed2 + ... + b6*TSseed6 + confounds
+            print('First level analysis -----')
+            print('TSvox = b0 + b1*TSseed1 + b2*TSseed2 + ... + b6*TSseed6')
 
-                #DROP 4 initial volumes and generate confounds matrix
-                drop_confound_df = confounds_df.loc[4:].values #from confounds
-                drop_aroma_conf = aroma_conf.loc[3:].values #it has one less time point
+            # Add a constant term to the independent variables
+            design_matrix = TS_seeds
+            design_matrix = sm.add_constant(design_matrix)
 
-                my_confounds = np.concatenate((drop_confound_df, drop_aroma_conf), axis=1)
+            for vox in range(len(idx_GM)):
+                idx_fullvol = idx_GM[vox]
+                # Fit the multiple linear regression model
+                model = sm.OLS(func_2d[:, idx_fullvol], design_matrix)
+                results = model.fit()
+                # Get the t-values
+                t_values = results.tvalues
+                t[vox, :] = t_values[1:7]   # t[voxels,seeds]
 
-                #DROP 4 volumes from bold data
-                raw_func_img = nimg.load_img(bold)
-                func_img = raw_func_img.slicer[:,:,:,4:]
-                dim4d = func_img.shape
-                N = dim4d[-1]  # number of time points
+            #SAVE tmaps as nifti files
 
-                # #reshape into 2d
-                func_2d = func_img.get_fdata().reshape(-1, func_img.shape[-1]).T
+            #Generate T-map using GMmask affine info
+            tmap_img = Vgm_img
+            tmap = tmap_img.get_fdata()
 
-                # pre-allocate space for seeds' time series
-                TS_seeds = np.empty((N, len(seednames)))
-                # Input new nifti for seeds
-                # Compute SEEDs TS
-                for k in range(len(seednames)):
-                    print('----- TS Seed:', seednames[k], '-----')
+            for t_index in range(t.shape[1]):
+                tmap = tmap.reshape(-1, np.prod(dim3d)) #reshape to 2D
+                tmap[:] = 0 # clean img
+                tmap[0,idx_GM] = t[:,t_index] #put t-values for each seed; seed 1 (t[:,0])
+                tmap = tmap.reshape(dim3d) #reshape to 3D
 
-                    seed = os.path.join(masks, seednames[k])
-                    #load seed image
-                    Vseed_img = nib.load(seed)
-                    Vseed = Vseed_img.get_fdata().reshape(func_2d.shape[-1])
+                #Generate nifti object
+                tmap_img = nib.Nifti1Image(tmap, tmap_img.affine)
 
-                    #for each time point, compute mean of voxels in which seed==1
-                    for vol in range(N):
-                        Vaux = func_2d[vol, :]
-                        TS_seeds[vol, k] = np.mean(Vaux[Vseed == 1])
-                        del Vaux
+                tmap_metadata = {
+                    'task': 'rest',
+                    'suffix': 'tstat',
+                    'extension': '.nii.gz',
+                    'session': str(ses),
+                    'subject': 'sub-'+i,
+                    'space': 'MNI152',
+                    'seed': seednames[t_index].split("_")[0]
+                }
 
-                    del Vseed
+                ##uncomment to plot tmap on top of T1 image
+                #t1_img = nib.load(anat)
+                #display=nplot.plot_anat(t1_img)
+                #display.add_overlay(tmap_img)
 
-                # 1st Level Analysis: TSvox = b0 + b1*TSseed1 + b2*TSseed2 + ... + b6*TSseed6 + confounds
-                print('First level analysis -----')
-                print('TSvox = b0 + b1*TSseed1 + b2*TSseed2 + ... + b6*TSseed6 + CSF + WM + cosines + AROMA_noiseICs')
+                # Save each tmap as a nifti file
+                filepath = os.path.join(output,tmap_metadata["subject"],"ses-"+tmap_metadata['session'],"func")
+                filename = tmap_metadata["subject"] + "_" + \
+                            "ses-"+tmap_metadata['session'] + "_" + \
+                            "task-"+tmap_metadata['task'] + "_" + \
+                            "space-"+tmap_metadata['space'] + "_" + \
+                            "seed-"+tmap_metadata['seed'] + "_" + \
+                            tmap_metadata["suffix"] + \
+                            tmap_metadata["extension"]
 
-                # Add a constant term to the independent variables
-                design_matrix = np.concatenate((TS_seeds,my_confounds), axis=1)
-                design_matrix = sm.add_constant(design_matrix)
+                folder_subj=os.path.join(output,tmap_metadata["subject"])
+                if not os.path.exists(folder_subj):
+                    os.makedirs(folder_subj)
 
-                for vox in range(len(idx_GM)):
-                    idx_fullvol = idx_GM[vox]
-                    # Fit the multiple linear regression model
-                    model = sm.OLS(func_2d[:, idx_fullvol], design_matrix)
-                    results = model.fit()
-                    # Get the t-values
-                    t_values = results.tvalues
-                    t[vox, :] = t_values[1:7]   # t[voxels,seeds]
+                if not os.path.exists(filepath):
+                     os.makedirs(filepath)
 
-                #SAVE tmaps as nifti files
+                tmap_img.to_filename(os.path.join(filepath, filename))
 
-                #Generate T-map using brainmask affine info
-                Vgm_img=nimg.load_img(brainmask)
-                tmap_img = Vgm_img
-                tmap = tmap_img.get_fdata()
-
-                for t_index in range(t.shape[1]):
-                    tmap = tmap.reshape(-1, np.prod(dim3d)) #reshape to 2D
-                    tmap[:] = 0 # clean img
-                    tmap[0,idx_GM] = t[:,t_index] #put t-values for each seed; seed 1 (t[:,0])
-                    tmap = tmap.reshape(dim3d) #reshape to 3D
-
-                    #Generate nifti object
-                    tmap_img = nib.Nifti1Image(tmap, tmap_img.affine)
-
-                    tmap_metadata = {
-                        'task': 'rest',
-                        'suffix': 'tstat',
-                        'extension': '.nii.gz',
-                        'run': str(r),
-                        'session': str(ses),
-                        'subject': 'sub-'+i,
-                        'space': 'MNI152NLin2009cAsym',
-                        'seed': seednames[t_index].split("_")[0]
-                    }
-
-                    ##uncomment to plot tmap on top of T1 image
-                    #t1_img = nib.load(anat)
-                    #display=nplot.plot_anat(t1_img)
-                    #display.add_overlay(tmap_img)
-
-                    # Save each tmap as a nifti file
-                    filepath = os.path.join(output,tmap_metadata["subject"],"ses-"+tmap_metadata['session'],"func")
-                    filename = tmap_metadata["subject"] + "_" + \
-                                "ses-"+tmap_metadata['session'] + "_" + \
-                                "task-"+tmap_metadata['task'] + "_" + \
-                                "run-"+tmap_metadata['run'] + "_" + \
-                                "space-"+tmap_metadata['space'] + "_" + \
-                                "seed-"+tmap_metadata['seed'] + "_" + \
-                                tmap_metadata["suffix"] + \
-                                tmap_metadata["extension"]
-
-                    folder_subj=os.path.join(output,tmap_metadata["subject"])
-                    if not os.path.exists(folder_subj):
-                        os.makedirs(folder_subj)
-
-                    if not os.path.exists(filepath):
-                         os.makedirs(filepath)
-
-                    tmap_img.to_filename(os.path.join(filepath, filename))
-
-                print('\t\t ----- Finished subject', i, ' Sess: ' ,ses,' Run: ',r,' ----')
+            print('\t\t ----- Finished subject', i, ' Sess: ' ,ses,'----')
 
     #%%
 if __name__ == '__main__':
--- LongitudinalPLOT/Plot.R
+++ LongitudinalPLOT/Plot.R
@@ -3,32 +3,22 @@
 
 # Set environment ####
 rm(list = ls()) # ctrl + L to clear console
-setwd("/Volumes/PortableSSD/PostdocUC_projects/FEP_Longitudinal/")
+setwd("~/Documents/GitHub/Striatocortical-connectivity-FEPtrt/LongitudinalPLOT/")
 library(tidyverse)
-
 
 # PREPARE DATA ####
 ## Import data ####  
-sample<-read.csv("patients_longitudinal.csv")
-sample_HC<-read.csv("controls_longitudinal.csv")
-
+sample<-read.csv("subjects_PEP_Jan2024.csv")
 #keep only columns and rows of interest
 df <- sample[, c("ID", "Grupo","Fecha_nacimiento","Sexo", 
-                          "MRI_1","MRI_2","MRI_3","MRI_4",
-                          "DUP.months.","DIT_preSess1.days.",
-                          "DIT_sess1toLastsess","Duration_of_illness.months.",
-                          "Excluir")]
-df <- df[1:117,] #ROWS up to subject 1517
-rm(sample) #remove original full df
-
-#same for controls
-df_HC <- sample_HC[, c("ID", "Grupo","Fecha_nacimiento","Sexo", 
                  "MRI_1","MRI_2","MRI_3","MRI_4",
+                 "DUP.months.","DIT_preSess1.days.",
+                 "DIT_sess1toLastsess","Duration_of_illness.months.",
                  "Excluir")]
-df_HC <- df_HC[1:88,] #ROWS up to subject C87
 
-rm(sample_HC)
-
+#keep ROWS up to subject 1517
+df <- df[1:117,] 
+rm(sample) #remove original full df
 
 ## Change data classes ####
 ### PATIENTS ####
@@ -63,20 +53,6 @@ df$Duration_of_illness.months. <- as.numeric(df$Duration_of_illness.months.)
 
 sapply(df, class)
 
-### CONTROLS####
-#Correct class and format of dates (Birth and MRI dates)
-df_HC$Fecha_nacimiento <- as.Date(df_HC$Fecha_nacimiento, format = "%d/%m/%Y")
-df_HC$MRI_1 <- as.Date(df_HC$MRI_1, format = "%d/%m/%Y")
-df_HC$MRI_2 <- as.Date(df_HC$MRI_2, format = "%d/%m/%Y")
-df_HC$MRI_3 <- as.Date(df_HC$MRI_3, format = "%d/%m/%Y")
-df_HC$MRI_4 <- as.Date(df_HC$MRI_4, format = "%d/%m/%Y")
-
-#Factors: ID, Group (TRT, nonTRT), Sex
-df_HC$ID <- as.factor(df_HC$ID)
-df_HC$Grupo <- as.factor(df_HC$Grupo)
-df_HC$Sexo <- as.factor(df_HC$Sexo)
-
-sapply(df_HC, class)
 
 ## Exclude and remove NA ####
 #PATIENTS
@@ -87,23 +63,58 @@ df <- df[!is.na(df$Sexo), ]
 df <- df[!is.na(df$DUP.months.), ]
 df <- df[!is.na(df$DIT_preSess1.days.), ]
 
-#CONTROLS
-df_HC <- df_HC[!df_HC$Excluir==1,]
-
-df_HC <- df_HC[!is.na(df_HC$Fecha_nacimiento), ]
-df_HC <- df_HC[!is.na(df_HC$Sexo), ]
-
-
-## Correct DATA ERRORS ####
-df[df$ID=="1439","Fecha_nacimiento"] <- as.Date("11/07/1996",format = "%d/%m/%Y" )
-
 
 #esta es la opción de incluirlos haciendo cero los NA
 #df$DUP.months.[is.na(df$DUP.months.)] <- 0  
 
 
+# PLOT 1a: DURATION IN TREATMENT####
+## Define time-points for plot ####
 
-# PLOT 1: Duration of illness ####
+# t0 = 0 : inicio de enfermedad
+# t1: time from illness start to first MRI scan
+#t1 = DUP + DIT_preSess1.days 
+#df$t1 <- df$DUP.months. + df$DIT_preSess1.days./30
+
+#t1 = DIT (duration in treatment preSess1)
+df$t1 <- df$DIT_preSess1.days./30
+
+#time difference in "months" = weeks/4
+df$t2<- df$t1 + as.numeric(difftime(df$MRI_2, df$MRI_1, units = 'weeks'))/4  
+df$t3<- df$t1 + as.numeric(difftime(df$MRI_3, df$MRI_1, units = 'weeks'))/4
+df$t4<- df$t1 + as.numeric(difftime(df$MRI_4, df$MRI_1, units = 'weeks'))/4
+
+## Reorder by t1####
+df <- df[order(df$t1), ]
+row.names(df) <- NULL
+
+df$plotID<-as.numeric(row.names(df))
+specific_ticks <- c(12, 12*2, 12*4, 12*6, 12*7)
+specific_labels <- c("","","","","")
+
+## Plot ####
+ggplot(df %>% 
+         pivot_longer(cols=starts_with("t"),
+                      names_to = c(".value", "sess"),
+                      names_pattern = "(.+)(.)") %>%
+         drop_na(t) %>% 
+         mutate(sess = as.numeric(sess)), 
+       aes(x = t)) +
+  geom_line(aes(y=reorder(ID, plotID), group = ID ), size = .3) +
+  geom_point(aes(y=reorder(ID, plotID), color = as.factor(Grupo),shape = factor(sess)), size=2) +
+  labs(x = "Duration of treatment (months)", y = "Subject ID", color = "Group", shape= "Session") +
+  theme(legend.position = "top", axis.text.y = element_text(size = 5)) +
+  geom_density(aes(y = -..density..*1000), fill= "#69b3a2") +
+  geom_label(aes(x=75, y=-10, label="density")) + 
+  scale_x_continuous(breaks = c(specific_ticks, 0, 25, 50, 75, 100),
+                     labels = c(specific_labels, 0, 25, 50, 75, 100)) +
+  geom_text(aes(x=12, y=-20, label="1y"), size=3) +
+  geom_text(aes(x=12*2, y=-20, label="2y"), size=3) +
+  geom_text(aes(x=12*4, y=-20, label="4y"), size=3) +
+  geom_text(aes(x=12*6, y=-20, label="6y"), size=3) +
+  geom_text(aes(x=12*7, y=-20, label="7y"), size=3)
+
+# PLOT 1b: DURATION OF ILLNESS####
 ## Define time-points for plot ####
 
 # t0 = 0 : inicio de enfermedad
@@ -121,10 +132,10 @@ df <- df[order(df$t1), ]
 row.names(df) <- NULL
 
 df$plotID<-as.numeric(row.names(df))
-specific_ticks <- c(12, 12*2, 12*4, 12*6, 12*8)
+specific_ticks <- c(12, 12*2, 12*4, 12*6, 12*7)
 specific_labels <- c("","","","","")
 
-## Plot####
+## Plot ####
 ggplot(df %>% 
          pivot_longer(cols=starts_with("t"),
                       names_to = c(".value", "sess"),
@@ -144,118 +155,10 @@ ggplot(df %>%
   geom_text(aes(x=12*2, y=-20, label="2y"), size=3) +
   geom_text(aes(x=12*4, y=-20, label="4y"), size=3) +
   geom_text(aes(x=12*6, y=-20, label="6y"), size=3) +
-  geom_text(aes(x=12*8, y=-20, label="8y"), size=3)
+  geom_text(aes(x=12*7, y=-20, label="7y"), size=3)
 
 
+# Export the data frame to a CSV file
+write.csv(df, file = "subjects_finallist.csv", row.names = FALSE)
 
-
-# PLOT 2: Age : ONLY PATIENTS####
-## Define time-points for plot ####
-
-# age0 = 0  : birth
-# age1: age at sess1
-df$age1 <- as.numeric(difftime(df$MRI_1, df$Fecha_nacimiento, units = 'weeks'))/52  
-
-#time difference in "years" = weeks/52
-df$age2 <-df$age1 + as.numeric(difftime(df$MRI_2, df$MRI_1, units = 'weeks'))/52
-df$age3 <-df$age1 + as.numeric(difftime(df$MRI_3, df$MRI_1, units = 'weeks'))/52
-df$age4 <-df$age1 + as.numeric(difftime(df$MRI_4, df$MRI_1, units = 'weeks'))/52
-
-## Reorder by age1####
-df <- df[order(df$age1), ]
-row.names(df) <- NULL
-df$plotID<-as.numeric(row.names(df))
-
-## Plot####
-ggplot(df %>% 
-         pivot_longer(cols=starts_with("age"),
-                      names_to = c(".value", "sess"),
-                      names_pattern = "(.+)(.)") %>%
-         drop_na(age) %>% 
-         mutate(sess = as.numeric(sess)), 
-       aes(x = age)) +
-  geom_line(aes(y=reorder(ID, plotID), group = ID ), size = .3) +
-  geom_point(aes(y=reorder(ID, plotID), color = as.factor(Grupo),shape = factor(sess)), size=2) +
-  labs(x = "Age (y)", y = "Subject ID", color = "Group", shape= "Session") +
-  theme(legend.position = "top", axis.text.y = element_text(size = 5)) +
-  geom_density(aes(y = -..density..*100, group = Grupo, fill = Grupo, alpha=0.5)) +
-  guides(fill = "none", alpha = "none") +
-  geom_label(aes(x=33, y=-5, label="density"))
-
-
-
-
-# PLOT 3: Age : ONLY CONTROLS####
-## Define time-points for plot ####
-
-# age0 = 0  : birth
-# age1: age at sess1
-df_HC$age1 <- as.numeric(difftime(df_HC$MRI_1, df_HC$Fecha_nacimiento, units = 'weeks'))/52  
-
-#time difference in "years" = weeks/52
-df_HC$age2 <-df_HC$age1 + as.numeric(difftime(df_HC$MRI_2, df_HC$MRI_1, units = 'weeks'))/52
-df_HC$age3 <-df_HC$age1 + as.numeric(difftime(df_HC$MRI_3, df_HC$MRI_1, units = 'weeks'))/52
-df_HC$age4 <-df_HC$age1 + as.numeric(difftime(df_HC$MRI_4, df_HC$MRI_1, units = 'weeks'))/52
-
-## Reorder by age1####
-df_HC <- df_HC[order(df_HC$age1), ]
-row.names(df_HC) <- NULL
-df_HC$plotID<-as.numeric(row.names(df_HC))
-
-## Plot####
-ggplot(df_HC %>% 
-         pivot_longer(cols=starts_with("age"),
-                      names_to = c(".value", "sess"),
-                      names_pattern = "(.+)(.)") %>%
-         drop_na(age) %>% 
-         mutate(sess = as.numeric(sess)), 
-       aes(x = age)) +
-  geom_line(aes(y=reorder(ID, plotID), group = ID ), size = .3) +
-  geom_point(aes(y=reorder(ID, plotID), color = as.factor(Grupo),shape = factor(sess)), size=2) +
-  labs(x = "Age (y)", y = "Subject ID", color = "Group", shape= "Session") +
-  theme(legend.position = "top", axis.text.y = element_text(size = 5)) +
-  geom_density(aes(y = -..density..*100, group = Grupo, fill = Grupo, alpha=0.5)) +
-  guides(fill = "none", alpha = "none") +
-  geom_label(aes(x=33, y=-5, label="density"))
-
-
-
-
-
-
-# PLOT 4: Age : FULL DATA SAMPLE####
-
-df_full <- rbind(df[,1:8], df_HC[1:8])
-
-## Define time-points for plot ####
-
-# age0 = 0  : birth
-# age1: age at sess1
-df_full$age1 <- as.numeric(difftime(df_full$MRI_1, df_full$Fecha_nacimiento, units = 'weeks'))/52  
-
-#time difference in "years" = weeks/52
-df_full$age2 <-df_full$age1 + as.numeric(difftime(df_full$MRI_2, df_full$MRI_1, units = 'weeks'))/52
-df_full$age3 <-df_full$age1 + as.numeric(difftime(df_full$MRI_3, df_full$MRI_1, units = 'weeks'))/52
-df_full$age4 <-df_full$age1 + as.numeric(difftime(df_full$MRI_4, df_full$MRI_1, units = 'weeks'))/52
-
-## Reorder by age1####
-df_full <- df_full[order(df_full$age1), ]
-row.names(df_full) <- NULL
-df_full$plotID<-as.numeric(row.names(df_full))
-
-## Plot####
-ggplot(df_full %>% 
-         pivot_longer(cols=starts_with("age"),
-                      names_to = c(".value", "sess"),
-                      names_pattern = "(.+)(.)") %>%
-         drop_na(age) %>% 
-         mutate(sess = as.numeric(sess)), 
-       aes(x = age)) +
-  geom_line(aes(y=reorder(ID, plotID), group = ID ), size = .3) +
-  geom_point(aes(y=reorder(ID, plotID), color = as.factor(Grupo),shape = factor(sess)), size=2) +
-  labs(x = "Age (y)", y = "Subject ID", color = "Group", shape= "Session") +
-  theme(legend.position = "top", axis.text.y = element_text(size = 5)) +
-  geom_density(aes(y = -..density..*100, group = Grupo, fill = Grupo, alpha=0.5)) +
-  guides(fill = "none", alpha = "none") +
-  geom_label(aes(x=33, y=-5, label="density"))
 
