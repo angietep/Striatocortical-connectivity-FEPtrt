a02edc726d9a41bd53cfee929ba5da1641f24855
BrainSur Lab <brainsur@201-75-115-eduroam-pf.wlan.net.uc.cl>
2024-02-22 16:02:02 -0300

    afni path

--- clustercorrection.py
+++ clustercorrection.py
@@ -48,83 +48,85 @@ def parse():
 
 #%%
 def main ():
-    #os.environ["ROOTDIR"] = '/Users/brainsur/Desktop'  # seth path
-    os.environ["ROOTDIR"] = '/Volumes/TOSHIBA'  # seth path
+    os.environ["ROOTDIR"] = '/Users/brainsur/Desktop'  # seth path
+    # os.environ["ROOTDIR"] = '/Volumes/TOSHIBA'  # seth path
     rootdir = os.environ["ROOTDIR"]
     if hasattr(sys, "ps1"):
         options = {}
-        workdir = os.path.join(rootdir,"striatconnTRT")
-        masks = os.path.join(workdir,"masks")
-        firstleveldir = os.path.join(workdir,"firstlevel") #  
-    
-        output  = workdir
+        workdir = os.path.join(rootdir, "striatconnTRT")
+        masks = os.path.join(workdir, "masks")
+        firstleveldir = os.path.join(workdir, "firstlevel")
+
+        output = workdir
         participants = []
 
-    else :
+    else:
         options = parse()
         participants = options.participants
         workdir = options.workdir
-        #rawdata = options.rawdata
-        #derivat = options.derivatives
+        # rawdata = options.rawdata
+        # derivat = options.derivatives
         script_path = os.path.dirname(__file__)
 
-
     print('firstlevel: ', firstleveldir)
-      
-                 
 
     #################
-    #Vgm_nii = nib.load(os.path.join(masks,'GrayMattermask_thalamus_space-MNI152_dim-9110991.nii.gz'))
-    #read vol
-    #Vgm_vol = Vgm_nii.get_fdata()
-    #save origianl dimensions (voxels_x, voxels_y, voxels_z)
-    #dim3d = Vgm_vol.shape
-    #reshape to 2D
-    #Vgm_2d = Vgm_vol.reshape(-1, np.prod(dim3d)).T  # -1 means auto-calculate size of dimension
-    #save indexes in which Vgm == 1 (indexes for gray matter location)
-    #idx_GM = np.where(Vgm_2d)[0]
+    # Vgm_nii = nib.load(os.path.join(masks,'GrayMattermask_thalamus_space-MNI152_dim-9110991.nii.gz'))
+    # read vol
+    # Vgm_vol = Vgm_nii.get_fdata()
+    # save origianl dimensions (voxels_x, voxels_y, voxels_z)
+    # dim3d = Vgm_vol.shape
+    # reshape to 2D
+    # Vgm_2d = Vgm_vol.reshape(-1, np.prod(dim3d)).T  # -1 means auto-calculate size of dimension
+    # save indexes in which Vgm == 1 (indexes for gray matter location)
+    # idx_GM = np.where(Vgm_2d)[0]
 
-    bidslayout = bids.BIDSLayout(firstleveldir, validate = False) #With validate = True it doesn't find any subjects
-    #confoundslayout = bids.BIDSLayout(rawdata, validate = False) #With validate = True it doesn't find any subjects
+    # With validate = True it doesn't find any subjects
+    bidslayout = bids.BIDSLayout(firstleveldir, validate=False)
+    # confoundslayout = bids.BIDSLayout(rawdata, validate = False) #With validate = True it doesn't find any subjects
 
     if not participants:
-        participants = bidslayout.get_subjects() #toma los del tsv y de algunos no tengo imágenes
-        #participants_sub = ["sub-" + item for item in participants]
+        # toma los del tsv y de algunos no tengo imágenes
+        participants = bidslayout.get_subjects()
+        # participants_sub = ["sub-" + item for item in participants]
 
     # Define the output DataFrame
-    output_df = pd.DataFrame(columns=['subject', 'ses','acf_x','acf_y','acf_z'])
-  
+    output_df = pd.DataFrame(
+        columns=['subject', 'ses', 'acf_x', 'acf_y', 'acf_z'])
+
     for p in participants:
-        #print(f"Subject: {p}")
+        # print(f"Subject: {p}")
         p = p.replace("sub-", "")
         for ses in bidslayout.get_sessions(subject=p):
-            #print(f"Session: {ses}")
+            # print(f"Session: {ses}")
             residuals = bidslayout.get(subject=p,
-                         session=ses,
-                         extension=".nii.gz",
-                         suffix="residuals",
-                         space="MNI152",
-                         #regex_search=True,
-                         #seed="DCPutamen", #does not work
-                         #invalid_filters="allow"
-                                 )
+                                       session=ses,
+                                       extension=".nii.gz",
+                                       suffix="residuals",
+                                       space="MNI152",
+                                       # regex_search=True,
+                                       # seed="DCPutamen", #does not work
+                                       # invalid_filters="allow"
+                                       )
             if len(residuals) < 1:
                 continue
             print(f"Subject: {p}, Session: {ses}, Residuals: {residuals}")
 
             # Replace 'your_command_here' with the actual 3dclustsim command and its arguments
-            afni_command = ['3dFWHMx', '-input', residuals[0].path]
- 
-            
+            afni_command = ['/Users/brainsur/abin/3dFWHMx',
+                            '-input', residuals[0].path]
+
             try:
-                result = subprocess.run(afni_command, check=True, capture_output=True, text=True)
+                result = subprocess.run(
+                    afni_command, check=True, capture_output=True, text=True)
                 # Print the output if needed
                 # Extract the three numbers from the result.stdout
                 values = [float(val) for val in result.stdout.split()[4:7]]
 
                 # Append the results to the DataFrame
-                #output_df = output_df.append({'subject': p,'ses': ses, 'acf_x': values[0], 'acf_y': values[1], 'acf_z': values[2]}, ignore_index=True)
-                output_df = pd.concat([output_df, pd.DataFrame({'subject': [p], 'ses': [ses], 'acf_x': [values[0]], 'acf_y': [values[1]], 'acf_z': [values[2]]})], ignore_index=True)
+                # output_df = output_df.append({'subject': p,'ses': ses, 'acf_x': values[0], 'acf_y': values[1], 'acf_z': values[2]}, ignore_index=True)
+                output_df = pd.concat([output_df, pd.DataFrame({'subject': [p], 'ses': [ses], 'acf_x': [
+                                      values[0]], 'acf_y': [values[1]], 'acf_z': [values[2]]})], ignore_index=True)
 
             except subprocess.CalledProcessError as e:
                 # Print the error if the command fails
