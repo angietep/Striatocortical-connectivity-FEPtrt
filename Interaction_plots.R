1bae960c7e093067e523455ed917909bcec04fc1
BrainSur Lab <brainsur@201-110-163-eduroam-pf.wlan.net.uc.cl>
2024-05-22 17:32:27 -0400

    demographics

--- Interaction_plots.R
+++ Interaction_plots.R
@@ -8,10 +8,12 @@ setwd("/Users/brainsur/Desktop/striatconnTRT")
 library(ggplot2)
 library(geomtextpath)
 library(ggsci)
+library(dplyr)
 # PREPARE DATA ####
 ## Import data ####  
 sample<-read.csv("tvals_cleansample_covars.csv")
 
+## Interaction PLOTS ####
 ggplot(sample, aes(x = t_DIT, y = tvals_SupVentralCaudate_timexTRS_cluster.1_size_721, color = group, group = ID)) +
   geom_point() +
   geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
@@ -26,60 +28,36 @@ ggplot(sample, aes(x = t_DIT, y = tvals_SupVentralCaudate_timexTRS_cluster.2_siz
   theme_bw() +
   scale_color_jama()
 
-ggplot(sample, aes(x = t_DIT, y = tvals_SupVentralCaudate_time_cluster.1_size_644, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
-
-ggplot(sample, aes(x = t_DIT, y = tvals_SupVentralCaudate_time_cluster.2_size_504, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
-
-ggplot(sample, aes(x = t_DIT, y = tvals_VRPutamen_time_cluster.1_size_750, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
-
-ggplot(sample, aes(x = t_DIT, y = tvals_DCPutamen_time_cluster.1_size_441, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
-
-ggplot(sample, aes(x = t_DIT, y = tvals_InfVentralCaudate_time_cluster.1_size_595, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
-
-ggplot(sample, aes(x = t_DIT, y = tvals_DorsalCaudate_TRS_cluster.1_size_385, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
-
-ggplot(sample, aes(x = t_DIT, y = tvals_InfVentralCaudate_HC_cluster.1_size_1100, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
-
-ggplot(sample, aes(x = t_DIT, y = tvals_DorsalCaudate_HC_cluster.1_size_501, color = group, group = ID)) +
-  geom_point() +
-  geom_line(size = 0.5, alpha = 0.7,linetype = "dotted") +  # Thinner lines connecting points of the same subject
-  geom_smooth(method = "lm", se = FALSE, size = 2, aes(group = group)) +  # Overall trend lines for each group
-  theme_bw() +
-  scale_color_jama()
 
 
+
+
+
+
+## Demographics and statistics ####
+
+df_last_session <- sample %>%
+  group_by(ID) %>%
+  filter(ses == max(ses)) %>%
+  ungroup()
+
+follow_ups <- df_last_session %>%
+   filter(ses!=1) %>%
+   ungroup()
+
+boxplot(t_DIT ~ group, data = follow_ups, main = "Follow-ups", ylab = "Group", xlab = "Follow-up time (months)", col = "lightblue", horizontal = TRUE)
+
+median(follow_ups$t_DIT[follow_ups$group == "HC"])
+IQR(follow_ups$t_DIT[follow_ups$group == "HC"])
+quantile(follow_ups$t_DIT[follow_ups$group == "HC"], 0.25)
+quantile(follow_ups$t_DIT[follow_ups$group == "HC"], 0.75)
+
+median(follow_ups$t_DIT[follow_ups$group == "TRS"])
+IQR(follow_ups$t_DIT[follow_ups$group == "TRS"])
+quantile(follow_ups$t_DIT[follow_ups$group == "TRS"], 0.25)
+quantile(follow_ups$t_DIT[follow_ups$group == "TRS"], 0.75)
+
+median(follow_ups$t_DIT[follow_ups$group == "nonTRS"])
+IQR(follow_ups$t_DIT[follow_ups$group == "nonTRS"])
+quantile(follow_ups$t_DIT[follow_ups$group == "nonTRS"], 0.25)
+quantile(follow_ups$t_DIT[follow_ups$group == "nonTRS"], 0.75)
